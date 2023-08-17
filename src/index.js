da896f25fe69da9feeb0bb2b478e8c8d273cc84e
AndreyS86 <108714524+AndreyS86@users.noreply.github.com>
2023-08-17 12:49:11 +0300

    instal librery

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import { hero } from './js/hero';
 import { favoritesRecepi } from './js/favorites-recepi';
 import { header } from './js/header';
 import { recepi } from './js/recepi';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
