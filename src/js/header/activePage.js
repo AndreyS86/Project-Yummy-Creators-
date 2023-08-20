1f84cfaba039c70ce895ec41304d084191be36fb
TetianaH67 <gutsalt9@gmail.com>
2023-08-20 20:06:31 +0300

    2
    upd

---
+++ src/js/header/activePage.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname.split("/").pop();
+  
+    const navLinks = document.querySelectorAll(".nav-link");
+  
+    navLinks.forEach(link => {
+      if (link.getAttribute("href").endsWith(currentPage)) {
+        link.classList.add("nav-link-active");
+        const currentSpan = document.createElement("span");
+        currentSpan.classList.add("current");
+        link.appendChild(currentSpan);
+  
+        link.addEventListener("focus", () => {
+          link.classList.add("nav-link-active");
+        });
+  
+        link.addEventListener("blur", () => {
+          link.classList.remove("nav-link-active");
+        });
+      }
+    });
+  });
+  
+  
+  // Mobmenu
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname.split("/").pop();
+  
+    const mobNavLinks = document.querySelectorAll(".mob-nav-link");
+  
+    mobNavLinks.forEach(link => {
+      if (link.getAttribute("href").endsWith(currentPage)) {
+        link.classList.add("mob-nav-link-active");
+      }
+  
+      link.addEventListener("focus", () => {
+        link.classList.add("mob-nav-link-active");
+      });
+  
+      link.addEventListener("blur", () => {
+        link.classList.remove("mob-nav-link-active");
+      });
+    });
+  });
+  
+  
