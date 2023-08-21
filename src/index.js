5aef2b59c041bac325a1d1478804cb627442ae17
AndreyS86 <108714524+AndreyS86@users.noreply.github.com>
2023-08-21 15:24:01 +0300

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import './js/category/category.js';
 import './js/search/search.js';
+import './js/header/activePage.js';
+import './js/header/mobMenu.js';
+import './js/header/darkTheme.js';
 import './js/favorites-recipe/favorites-recipe.js';
 import './js/hero/hero.js';
