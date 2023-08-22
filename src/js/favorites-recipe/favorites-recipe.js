66e3e15aa2ef97ed01ca84b589f147129320546f
AndreyS86 <108714524+AndreyS86@users.noreply.github.com>
2023-08-22 07:43:41 +0300

    fixed fractures and mob.btn

--- src/js/favorites-recipe/favorites-recipe.js
+++ src/js/favorites-recipe/favorites-recipe.js
@@ -2,5 +2,6 @@ import './fetch-dish-favorites.js';
 import './local-storage-favorites.js';
 import './markup-card-favorites.js';
 import '../header/darkTheme.js';
+import '../header/mobMenu.js';
 import '../scroll/up-scroll.js';
 import '../favorites-hero/favorites-button.js';
