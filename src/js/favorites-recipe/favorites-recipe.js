40e0503d37fb7cae99132c847fc5de1310e38b9a
Oleg Gonchar <oleg.vn89@gmail.com>
2023-08-22 03:51:03 +0300

    commit1

--- src/js/favorites-recipe/favorites-recipe.js
+++ src/js/favorites-recipe/favorites-recipe.js
@@ -3,3 +3,4 @@ import './local-storage-favorites.js';
 import './markup-card-favorites.js';
 import '../header/darkTheme.js';
 import '../scroll/up-scroll.js';
+import '../favorites-hero/favorites-button.js';
